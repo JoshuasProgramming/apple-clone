24447df3947d0c8808bfbe5e1da8e1b48bc99f2c
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-11-18 15:32:10 +0000

    Put banner underneath navbar and updated prev code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/header';
 import Ipad from './components/ipad';
 import MobileHeader from './components/mobileheader';
+import HolidayGift from './components/holidaygift';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MobileHeader/>
+      <HolidayGift/>
       <Ipad/>
     </div>
   );
---
+++ src/components/holidaygift.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/style.css';
+
+class HolidayGift extends Component {
+    render(){
+        return(
+            <section className="holiday-gift-section">
+                <p>Get your holiday gifts on time. See when to order</p>
+            </section>
+        )
+    }
+}
+
+export default HolidayGift;
