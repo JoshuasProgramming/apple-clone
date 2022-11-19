9dd98c737b47f7413e1adc4d072a0b707a5513b3
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-11-19 19:01:46 +0000

    Worked on the second part of the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,7 +12,100 @@ class Footer extends Component {
                 <div className="underline"></div>
 
                 <section className="sub-section-footer2">
+                    <div className="footer-list-containers">
+                        <ul>
+                            <li>Shop and Learn</li>
+                            <li>Store</li>
+                            <li>Mac</li>
+                            <li>iPad</li>
+                            <li>iPhone</li>
+                            <li>Watch</li>
+                            <li>AirPods</li>
+                            <li>TV & Home</li>
+                            <li>AirTag</li>
+                            <li>Accessories</li>
+                            <li>Gift Cards</li>
+                        </ul>
+                    </div>
 
+                    <div className="footer-list-containers">
+                        <ul>
+                            <li>Services</li>
+                            <li>Apple Music</li>
+                            <li>Apple TV+</li>
+                            <li>Apple Fitness+</li>
+                            <li>Apple News+</li>
+                            <li>Apple Arcade</li>
+                            <li>iCloud</li>
+                            <li>Apple One</li>
+                            <li>Apple Pay</li>
+                            <li>Apple Books</li>
+                            <li>Apple Podcasts</li>
+                            <li>Apple Store</li>
+                        </ul>
+
+                        <ul className='appleId'>
+                            <li>Manage Your Apple ID</li>
+                            <li>Apple Store Account</li>
+                            <li>iCloud.com</li>
+                        </ul>
+
+                    </div>
+                    <div className="footer-list-containers">
+                        <ul>
+                            <li>Apple Store</li>
+                            <li>Find a Store</li>
+                            <li>Genius Bar</li>
+                            <li>Today at Apple</li>
+                            <li>Apple Summer Camp</li>
+                            <li>Apple Store App</li>
+                            <li>Refurbished and Clearance</li>
+                            <li>Financing</li>
+                            <li>Apple Trade in</li>
+                            <li>Order Status</li>
+                            <li>Shopping Help</li>
+                        </ul>
+                    </div>
+                    <div className="footer-list-containers">
+                        <ul>
+                            <li>For Business</li>
+                            <li>Apple and Business</li>
+                            <li>Shop for Business</li>
+                        </ul>
+                        <ul>
+                            <li>For Education</li>
+                            <li>Apple and Education</li>
+                            <li>Shop for University</li>
+                        </ul>
+                        <ul className='healthcare'>
+                            <li>For Healthcare</li>
+                            <li>Apple in Healthcare</li>
+                            <li>Health on Apple Watch</li>
+                            <li>Health Records on iPhone</li>
+                        </ul>
+                    </div>
+                    <div className="footer-list-containers">
+                        <ul>
+                            <li>Apple Values</li>
+                            <li>Accessibility</li>
+                            <li>Education</li>
+                            <li>Environment</li>
+                            <li>Privacy</li>
+                            <li>Supplier Responsibility</li>
+                        </ul>
+                        <ul>
+                            <li>About Apple</li>
+                            <li>Newsroom</li>
+                            <li>Apple Leadership</li>
+                            <li>Career Opportunities</li>
+                            <li>Warranty</li>
+                            <li>Investors</li>
+                            <li>Ethics & Compliance</li>
+                            <li>Events</li>
+                            <li>European Job Creation</li>
+                            <li>Contact Apple</li>
+                        </ul>
+                    </div>
                 </section>
 
                 <section className="sub-section-footer3">
