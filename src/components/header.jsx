729d04cd018d9ceaa47313ea5a60c2eaee46dd8b
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-11-20 13:36:07 +0000

    Added comments & better map usage for header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,20 +7,32 @@ import bag from '../images/bag.svg';
 class Header extends Component {
     render(){
         
-        const navList = ["Store", "Mac", "iPad", "iPhone", "Watch", "AirPods", "TV & Home",
-                        "Only on Apple", "Accessories", "Support"]
-
+        //navList (array) contains the different navigation list items (Strings).
+        //We'll use a map to loop through each item and present it on the page.
+        //This avoids us individually typing in each <li></li>
+        const navList = [apple, "Store", "Mac", "iPad", "iPhone", "Watch", "AirPods", "TV & Home",
+                        "Only on Apple", "Accessories", "Support", search, bag];
         return(
             <nav id='navigation'>  
                 <ul>
-                    <li><img src={apple} alt="apple-logo"></img></li>
 
+                    {/* using a map to loop through each item in 'navList', and put it into a li element */}
+                    
                     {navList.map((value, key) => {
+                        
+                        //whenever the value contains and image & is a 'Search' icon... We'll return it within a li>img
+                        if((value === search) && (value.includes("svg"))){
+                            return <li><img src={search} alt="apple-search-icon" className='search-icon'></img></li>
+                        } 
+                        
+                        //whenever the value is just a svg... We'll return it within a li>img 
+                        else if(value.includes("svg")){
+                            return <li><img key={key} src={value} alt={value + "-icon"}></img></li>
+                        }
+                        
+                        //When all other conditions fail we'll just return the normal Strings in an li element 
                         return <li key={key}>{value}</li>
                     })}
-
-                    <li>< img src={search} alt="apple-search-icon" className="search-icon" ></img></li>
-                    <li><img src={bag} alt="apple-bag" /></li>
                 </ul>
             </nav>
         )
